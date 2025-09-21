853573813787954c0e1e93700f38b6b030586aff
officialmubashir77 <officialmubashir77@gmail.com>
2025-09-22 03:38:49 +0500

    global middlewares in express

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
+
+// Global middleware
+app.use((req, res, next) => {
+    console.log(`This is a middleware and method is  ${req.method} request for '${req.url}'`);
+    next();
+});
+
 app.get('/', (req, res) => {
-  res.send('This is the home page');
+    // res.send('This is the home page');
+    res.render('index', { title: 'Home Page', message: 'Welcome to the Home Page!' });
 });
 
 app.get('/about', (req, res) => {
